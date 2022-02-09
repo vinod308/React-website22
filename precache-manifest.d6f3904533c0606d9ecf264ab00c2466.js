eb991a398c288d41cd208792dd03a20604aeddd4
jatinsangwan96 <jatinsangwan1120@gmail.com>
2022-02-09 17:08:18 +0530

    Updates

---
+++ precache-manifest.d6f3904533c0606d9ecf264ab00c2466.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed2db9046d6847abb418dcb723719e0",
+    "url": "/vinod308/React-WebApp/index.html"
+  },
+  {
+    "revision": "2179f94e988d70851218",
+    "url": "/vinod308/React-WebApp/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "a3355ba3e519ebbc7548",
+    "url": "/vinod308/React-WebApp/static/css/main.592793f1.chunk.css"
+  },
+  {
+    "revision": "2179f94e988d70851218",
+    "url": "/vinod308/React-WebApp/static/js/2.b286d6a8.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/vinod308/React-WebApp/static/js/2.b286d6a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3355ba3e519ebbc7548",
+    "url": "/vinod308/React-WebApp/static/js/main.e67f8c0c.chunk.js"
+  },
+  {
+    "revision": "70096648ede98d8c86ce",
+    "url": "/vinod308/React-WebApp/static/js/runtime-main.3bd79bb0.js"
+  },
+  {
+    "revision": "94b6feea48c06dd8d1ce15a0b952ed65",
+    "url": "/vinod308/React-WebApp/static/media/img4.94b6feea.jpg"
+  },
+  {
+    "revision": "87356bb1ad4308a7d4639a267da6f13a",
+    "url": "/vinod308/React-WebApp/static/media/img5.87356bb1.jpg"
+  },
+  {
+    "revision": "f677f4fe782bc13717c8df99aae008f8",
+    "url": "/vinod308/React-WebApp/static/media/img6.f677f4fe.jpg"
+  },
+  {
+    "revision": "4aefeea9954987cfc3b9f344a3cc154c",
+    "url": "/vinod308/React-WebApp/static/media/img7.4aefeea9.jpg"
+  },
+  {
+    "revision": "e6b8bc5b72d27e72c748555dfa08546f",
+    "url": "/vinod308/React-WebApp/static/media/img8.e6b8bc5b.jpg"
+  },
+  {
+    "revision": "81889773690ddbfbb17805302a65a02b",
+    "url": "/vinod308/React-WebApp/static/media/img9.81889773.jpg"
+  },
+  {
+    "revision": "b33ebee0e20ee3aab161821608336733",
+    "url": "/vinod308/React-WebApp/static/media/react2.b33ebee0.jpg"
+  },
+  {
+    "revision": "5001540476f6b71a5fd52c966241f072",
+    "url": "/vinod308/React-WebApp/static/media/react3.50015404.png"
+  }
+]);
